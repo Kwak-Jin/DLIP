90afde7c53cae0d94eaa9e302d9f7763bd51d632
jinkwak <104green@naver.com>
2024-05-05 03:46:02 +0900

    DLIP Python LAB3 1. Post Processing required Tested-by: jinkwak <104green@naver.com>

--- DLIP_Python/LAB/PyLAB1/mainVideo.py
+++ DLIP_Python/LAB/PyLAB1/mainVideo.py
@@ -12,7 +12,7 @@
 import cv2 as cv
 import numpy as np
 
-N_MedianIter = 20
+N_MedianIter = 25
 kernel = 5
 YOK = 1
 NOK = 0
@@ -21,8 +21,8 @@ level3 = 120
 """ ROI length and width"""
 ROI_y = 200
 ROI_h = 1080 -ROI_y
-ROI_x = 200
-ROI_w = 500
+ROI_x = 125
+ROI_w = 575
 
 
 def main():
@@ -45,7 +45,7 @@ def main():
 
         roi = frame[ROI_y:ROI_y + ROI_h, ROI_x:ROI_x + ROI_w]
         psrc = roi.copy()
-        pre_src = Preprocess(roi)
+        pre_src = preprocess(roi)
 
         fx1 = np.zeros((ROI_w), np.int16)
         fx = np.zeros((ROI_w), np.int16)
@@ -73,6 +73,7 @@ def main():
                 if pre_src[tdx, idx] == 255:
                     fx1[idx] = tdx  # row
                     frame = cv.circle(frame, (ROI_x + idx, ROI_y+fx1[idx]), 1, (255, 0, 255), -1)
+                    psrc = cv.circle(psrc, (idx, fx1[idx]), 1, (255, 0, 255), -1)
                     break
             xx1[idx] = idx
             if (idx<ROI_w-2) and (np.abs(fx1[idx] - fx1[idx + 1]) >= discontinuity):
@@ -96,6 +97,7 @@ def main():
         for idx in range(0, ROI_w):
             psrc = cv.circle(psrc, (idx, newFx[idx]), 1, (255, 255, 0), -1)
 
+        """ After Polyfit  """
         maxBend = max(newFx)
         #if maxBend < frame.shape[0] - level3 and maxBend >= frame.shape[0] - level2:
         if maxBend < ROI_h - level3 and maxBend >= ROI_h - level2:
@@ -105,7 +107,7 @@ def main():
         else:
             _str = "Level: 1"
 
-        frame = drawLine(frame)
+        frame = drawLevelBound(frame)
         bend  = 'Score:'+maxBend.astype(np.str_)
         cv.putText(frame, _str, ((int)(ROI_w),(int)(ROI_h/2)-50) , cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         cv.putText(frame, bend, ((int)(ROI_w),(int)(ROI_h/2)) , cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
@@ -116,7 +118,7 @@ def main():
     cv.destroyAllWindows()
     cap.release()
 
-def Preprocess(img: np.ndarray) -> np.ndarray:
+def preprocess(img: np.ndarray) -> np.ndarray:
     _dst = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     for i in range(0, N_MedianIter):
         _dst = cv.medianBlur(_dst, kernel)
@@ -135,7 +137,7 @@ def checkEnd(ret:bool) -> int:
         flag = YOK
     return flag
 
-def drawLine(img: np.ndarray) -> np.ndarray:
+def drawLevelBound(img: np.ndarray) -> np.ndarray:
     cv.line(img, (0, img.shape[0] - level3), (img.shape[1], img.shape[0] - level3), (255, 0, 255), 2,
             cv.LINE_AA)  # Level 3
     cv.line(img, (0, img.shape[0] - level2), (img.shape[1], img.shape[0] - level2), (255, 255, 0), 2,
@@ -143,4 +145,5 @@ def drawLine(img: np.ndarray) -> np.ndarray:
     return img
 
 if __name__ == '__main__':
+    print(__doc__)
     main()
